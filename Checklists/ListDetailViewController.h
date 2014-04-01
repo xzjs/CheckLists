cf317db99d7f52edc310a0458d25d2a6a77c082f
xzjs <xzjs@ap.local>
2014-04-01 18:48:00 +0800

    16章第8页，在alllist页面添加了一个add按钮

--- Checklists/ListDetailViewController.h
+++ Checklists/ListDetailViewController.h
@@ -14,10 +14,20 @@
 @protocol ListDetailViewControllerDelegate <NSObject>
 
 -(void)listDetailViewControllerDidCancel:(ListDetailViewController*)controller;
--(void)
+-(void)listDetailViewController:(ListDetailViewController*)controller didFinishAddingChecklist:(Checklist*)checklist;
+-(void)listDetailViewController:(ListDetailViewController*)controller didFinishEditingChecklist:(Checklist*)checklist;
 
 @end
 
-@interface ListDetailViewController : UITableViewController
+@interface ListDetailViewController : UITableViewController<UITextFieldDelegate>
+
+@property(nonatomic,weak)IBOutlet UITextField *textField;
+@property(nonatomic,weak)IBOutlet UIBarButtonItem *doneBarButton;
+@property(nonatomic,weak)id <ListDetailViewControllerDelegate> delegate;
+
+@property(nonatomic,strong)Checklist *checklistToEdit;
+
+-(IBAction)cancel:(id)sender;
+-(IBAction)done:(id)sender;
 
 @end
